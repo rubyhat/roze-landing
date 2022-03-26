9149244cc78a83dfdcdbe9df551d3421699ced06
Vladislav <simwebkz@gmail.com>
2022-03-26 17:55:46 +0600

    media & animation added

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
 import "../scss/index.scss";
 import swiperSlider from "./slider";
+import smoothscroll from "smoothscroll-polyfill";
+
+// kick off the polyfill!
+smoothscroll.polyfill();
 
 document.addEventListener("DOMContentLoaded", function () {
+  new WOW().init();
+
   swiperSlider.forEach((swiper) => {
     swiper.init();
   });
@@ -30,4 +36,45 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   };
   toggleTabs();
+
+  const preloader = document.querySelector(".preloader");
+  const preloaderContent = preloader.querySelector(".preloader__content");
+
+  if (!localStorage.was) {
+    localStorage.was = "true";
+    setTimeout(() => {
+      preloaderContent.classList.add("preloader__content_hidden");
+    }, 2500);
+
+    setTimeout(() => {
+      preloader.classList.add("preloader_hidden");
+    }, 3000);
+  } else {
+    preloader.classList.add("d-none");
+  }
+
+  if (!localStorage.nextDay) {
+    localStorage.nextDay = Math.floor((Date.now() + 3600 * 1000 * 24) / 1000);
+  } else {
+    Math.floor(Date.now() / 1000) >= parseInt(localStorage.nextDay)
+      ? localStorage.clear()
+      : null;
+  }
+
+  const smoothScroll = () => {
+    const links = document.querySelectorAll(".navigation__link");
+    for (const link of links) {
+      const linkData = link.getAttribute("data-section");
+      const section = document.querySelector(`.${linkData}`);
+
+      link.addEventListener("click", () => {
+        window.scroll({
+          behavior: "smooth",
+          left: 0,
+          top: section.offsetTop,
+        });
+      });
+    }
+  };
+  smoothScroll();
 });
